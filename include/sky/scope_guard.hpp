a47c10ab0a2a78a446b365839d9428017c23531f
Tianyu Zhu <tian.tian098@gmail.com>
2012-12-13 23:55:24 -0500

    Initial implementation of scope guard.

---
+++ include/sky/scope_guard.hpp
@@ -0,0 +1,127 @@
+#ifndef SCOPE_GUARD_HPP
+#define SCOPE_GUARD_HPP
+
+#include <utility>
+
+namespace sky {
+
+template<typename Routine>
+class _scope_guard;
+
+/**
+ * @brief Creates a scope guard on the stack.
+ *
+ * A scope guard is a lightweight RAII object that
+ * can perform arbitrary actions upon destruction,
+ * even in the face of exceptions.
+ *
+ * These actions may be dismissed (not performed
+ * at destruction) if the dismiss method is called
+ * on the scope guard.
+ *
+ * @param todo Guarded actions to perform upon
+ * destruction in the form of a callable object.
+ *
+ * @return An appropriate scope guard object,
+ * allocated unto the stack.
+ */
+template<typename Routine>
+__scope_guard<Routine>
+scope_guard(Routine &&todo);
+
+template<typename Routine>
+class __scope_guard
+{
+    // Copy and move constructors. Private use only.
+
+    explicit __scope_guard(Routine &);
+    explicit __scope_guard(Routine &&);
+
+    template<>
+    friend
+    __scope_guard<Routine>
+    scope_guard(Routine &&);
+
+public:
+
+    // Scope guards are moveable but not copyable
+
+    __scope_guard(__scope_guard &&g);
+    __scope_guard &operator =(__scope_guard &&g) noexcept;
+
+    __scope_guard(__scope_guard const&) = delete;
+    __scope_guard &operator =(__scope_guard const&) = delete;
+
+    /**
+     * Causes the destructor to no longer run guarded routine.
+     */
+    void dismiss() noexcept;
+
+    ~__scope_guard();
+
+private:
+    Routine todo;
+    bool perform = true;
+};
+
+template<typename Routine>
+__scope_guard<Routine>
+scope_guard(Routine &&todo)
+{
+    return __scope_guard<Routine>(
+                std::forward<Routine>(todo));
+}
+
+template<typename Routine>
+explicit
+__scope_guard<Routine>::
+__scope_guard(Routine &todo) :
+todo(todo)
+{}
+
+template<typename Routine>
+explicit
+__scope_guard<Routine>::
+__scope_guard(Routine &&todo) :
+todo(std::move(todo))
+{}
+
+template<typename Routine>
+__scope_guard<Routine>::
+__scope_guard(__scope_guard &&g) :
+todo(std::move(g.todo)),
+perform(g.perform)
+{
+    g.dismiss();
+}
+
+template<typename Routine>
+__scope_guard &
+__scope_guard<Routine>::
+operator =(__scope_guard &&g) noexcept
+{
+    using std::swap;
+    swap(todo, g.todo);
+    perform = g.perform;
+    g.dismiss();
+    return *this;
+}
+
+template<typename Routine>
+void
+__scope_guard<Routine>::
+dismiss() noexcept
+{
+    perform = false;
+}
+
+template<typename Routine>
+__scope_guard<Routine>::
+~__scope_guard()
+{
+    if (perform) todo();
+}
+
+} // namespace sky
+
+#endif // SCOPE_GUARD_HPP

5eab4f8c4031d9ef5c55e9ed0e174672d4217ff7
Tianyu Zhu <tian.tian098@gmail.com>
2013-01-09 20:18:03 -0500

    Initial backbone of concurrent_queue. Not thread-safe yet

--- include/sky/concurrent_queue.hpp
+++ include/sky/concurrent_queue.hpp
@@ -2,6 +2,8 @@
 #define CONCURRENT_QUEUE_HPP
 
 #include <queue>
+#include <utility>
+#include <type_traits>
 
 namespace sky {
 
@@ -36,24 +38,90 @@ public:
 
     concurrent_queue &operator =(concurrent_queue &&);
 
-    reference front();
-    const_reference front() const;
+    value_type pop();
 
-    void pop();
-
-    bool empty() const;
-
-    void push(const T& value);
+    void push(T const& value);
     void push(T&& value);
 
     template<typename... Args>
     void emplace(Args... args);
 
-    void swap(const concurrent_queue& other);
+    bool empty() const;
+
+    void swap(concurrent_queue const& other);
 
 private:
     container_type queue;
 };
+
+template<typename T>
+concurrent_queue<T>::
+concurrent_queue() {}
+
+template<typename T>
+template<typename Alloc>
+concurrent_queue<T>::
+concurrent_queue(Alloc &&alloc) :
+    queue(std::forward<Alloc>(alloc))
+{}
+
+template<typename T>
+concurrent_queue<T>::
+concurrent_queue(concurrent_queue &&other) :
+    queue(std::move(other.queue))
+{}
+
+template<typename T>
+concurrent_queue<T> &
+concurrent_queue<T>::
+operator =(concurrent_queue &&other)
+{
+    queue = std::move(other.queue);
+    return *this;
+}
+
+template<typename T>
+typename concurrent_queue<T>::value_type
+concurrent_queue<T>::
+pop()
+{
+    value_type value = queue.front();
+    queue.pop(); // Exception safe?
+    return value;
+}
+
+template<typename T>
+void
+concurrent_queue<T>::
+push(T const& value)
+{
+    queue.push(value);
+}
+
+template<typename T>
+void
+concurrent_queue<T>::
+push(T && value)
+{
+    queue.push(std::move(value));
+}
+
+template<typename T>
+bool
+concurrent_queue<T>::
+empty() const
+{
+    return queue.empty();
+}
+
+template<typename T>
+void
+concurrent_queue<T>::
+swap(concurrent_queue const& other)
+{
+    using std::swap;
+    swap(queue, other.queue);
+}
 
 } // namespace sky
 
--- test/concurrent_queue/concurrent_queue.cpp
+++ test/concurrent_queue/concurrent_queue.cpp
@@ -3,3 +3,8 @@
 #include "sky/concurrent_queue.hpp"
 
 using sky::concurrent_queue;
+
+TEST(ConcurrentQueue, Construct)
+{
+    concurrent_queue<int>();
+}

3f7d47ce638faedcbd22a0df5bd2df4028951b20
Tianyu Zhu <tian.tian098@gmail.com>
2013-02-15 19:13:32 -0500

    Initial implementation of atomic_counter

--- include/sky/atomic.hpp
+++ include/sky/atomic.hpp
@@ -2,6 +2,8 @@
 #define ATOMIC_HPP
 
 #include <atomic>
+#include <utility>
+#include <type_traits>
 
 namespace sky {
 
@@ -13,12 +15,16 @@ template<typename T>
  * variable designed to be used only for counting.
  * In particular, multiple "writer" threads are
  * allowed to blindly increment/decrement the
- * value of the variable. "Reader" will only see
- * a consistent view of the counter after all
+ * value of the variable. "Reader" threads will only
+ * see a consistent view of the counter after all
  * writer threads are finished writing.
  */
 class atomic_counter
 {
+    static_assert(std::is_integral<T>::value &&
+                  !std::is_same<T, bool>::value,
+                  "T must be a non-bool, integral type.");
+
 public:
 
     /**
@@ -27,15 +33,14 @@ public:
      */
     atomic_counter();
 
-    template <typename... Args>
     /**
-     * @brief Creates an atomic counter and
-     * emplaces the initial value of the counter.
+     * @brief Creates an atomic counter with some
+     * initial value.
      *
-     * @param args Arguments for the initial value
+     * @param val Arguments for the initial value
      *        of the counter.
      */
-    atomic_counter(Args... args);
+    atomic_counter(T val);
 
     /**
      * @brief Post-increment.
@@ -49,9 +54,9 @@ public:
 
     /**
      * @brief Increment by value.
-     * @param t The value to increment by.
+     * @param val The value to increment by.
      */
-    void operator +=(T const&t) const;
+    void operator +=(T val) const;
 
     /**
      * @brief Post-decrement.
@@ -65,24 +70,99 @@ public:
 
     /**
      * @brief Decrement by value.
-     * @param t The vlaue to decrement by.
+     * @param val The vlaue to decrement by.
      */
-    void operator -=(T const&t) const;
+    void operator -=(T val) const;
 
     /**
      * @brief Access the value of the counter.
      * @return The value of the counter.
      */
-    T const& get();
+    T load();
 
     /**
      * @brief The value of the counter.
      */
-    operator T const&();
+    operator T();
 
 private:
     mutable std::atomic<T> value;
 };
+
+template<typename T>
+atomic_counter<T>::
+atomic_counter() :
+    value()
+{}
+
+template<typename T>
+atomic_counter<T>::
+atomic_counter(T val) :
+    value(val)
+{}
+
+template<typename T>
+void
+atomic_counter<T>::
+operator ++() const
+{
+    operator +=(T(1));
+}
+
+template<typename T>
+void
+atomic_counter<T>::
+operator ++(int) const
+{
+    operator +=(T(1));
+}
+
+template<typename T>
+void
+atomic_counter<T>::
+operator +=(T val) const
+{
+    value.fetch_add(val, std::memory_order_relaxed);
+}
+
+template<typename T>
+void
+atomic_counter<T>::
+operator --() const
+{
+    operator -=(T(1));
+}
+
+template<typename T>
+void
+atomic_counter<T>::
+operator --(int) const
+{
+    operator -=(T(1));
+}
+
+template<typename T>
+void
+atomic_counter<T>::
+operator -=(T val) const
+{
+    value.fetch_sub(val, std::memory_order_relaxed);
+}
+
+template<typename T>
+T
+atomic_counter<T>::
+load()
+{
+    return value.load(std::memory_order_relaxed);
+}
+
+template<typename T>
+atomic_counter<T>::
+operator T()
+{
+    return load();
+}
 
 } // namespace sky
 
--- test/atomic/atomic_counter.cpp
+++ test/atomic/atomic_counter.cpp
@@ -1,1 +1,80 @@
+#include <gtest/gtest.h>
+
 #include <sky/atomic.hpp>
+
+using namespace sky;
+
+template<typename T>
+class AtomicCounter : public testing::Test {};
+
+typedef testing::Types<char, int, unsigned int, long> Integrals;
+
+TYPED_TEST_CASE(AtomicCounter, Integrals);
+
+TYPED_TEST(AtomicCounter, ConstructDefault)
+{
+    atomic_counter<TypeParam> t;
+
+    EXPECT_EQ(0, (TypeParam)t);
+}
+
+TYPED_TEST(AtomicCounter, ConstructEmplace)
+{
+    atomic_counter<TypeParam> t(23);
+
+    EXPECT_EQ(23, (TypeParam)t);
+}
+
+TYPED_TEST(AtomicCounter, PreIncrement)
+{
+    atomic_counter<TypeParam> t(0);
+
+    ++t;
+
+    EXPECT_EQ(1, (TypeParam)t);
+}
+
+TYPED_TEST(AtomicCounter, PostIncrement)
+{
+    atomic_counter<TypeParam> t(0);
+
+    t++;
+
+    EXPECT_EQ(1, (TypeParam)t);
+}
+
+TYPED_TEST(AtomicCounter, IncrementValue)
+{
+    atomic_counter<TypeParam> t(0);
+
+    t += 23;
+
+    EXPECT_EQ(23, (TypeParam)t);
+}
+
+TYPED_TEST(AtomicCounter, PreDecrement)
+{
+    atomic_counter<TypeParam> t(0);
+
+    --t;
+
+    EXPECT_EQ(-1, (TypeParam)t);
+}
+
+TYPED_TEST(AtomicCounter, PostDecrement)
+{
+    atomic_counter<TypeParam> t(0);
+
+    t--;
+
+    EXPECT_EQ(-1, (TypeParam)t);
+}
+
+TYPED_TEST(AtomicCounter, DecrementValue)
+{
+    atomic_counter<TypeParam> t(0);
+
+    t -= 23;
+
+    EXPECT_EQ(-23, (TypeParam)t);
+}

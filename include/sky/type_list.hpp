b087642c66783dd43ff1958cf09072503a1430cf
Tianyu Zhu <tian.tian098@gmail.com>
2013-03-14 16:44:32 -0400

    Initial implementation of type_list

--- include/sky/type_list.hpp
+++ include/sky/type_list.hpp
@@ -3,6 +3,28 @@
 
 namespace sky {
 
+template <typename... Types>
+class type_list
+{
+    template <typename First, typename... Rest>
+    struct non_empty_list
+    {
+        typedef First first;
+        typedef type_list<Rest...> rest;
+    };
+
+public:
+    static const bool empty = false;
+    typedef typename non_empty_list<Types...>::first first;
+    typedef typename non_empty_list<Types...>::rest rest;
+};
+
+template <>
+struct type_list<>
+{
+    static const bool empty = true;
+};
+
 } // namespace sky
 
 #endif // TYPE_LIST_HPP
--- test/type_list/type_list.cpp
+++ test/type_list/type_list.cpp
@@ -1,3 +1,38 @@
 #include "gtest/gtest.h"
 
 #include "sky/type_list.hpp"
+
+using sky::type_list;
+
+template <typename T, typename U>
+static void expect_eq()
+{
+    bool is_same = std::is_same<T,U>::value;
+    EXPECT_TRUE(is_same);
+}
+
+TEST(TypeList, Empty)
+{
+    typedef type_list<> list;
+    EXPECT_TRUE(list::empty);
+}
+
+TEST(TypeList, First)
+{
+    typedef type_list<int> list;
+    EXPECT_FALSE(list::empty);
+    expect_eq<int, list::first>();
+}
+
+TEST(TypeList, Rest)
+{
+    typedef type_list<int, long> list;
+    EXPECT_FALSE(list::empty);
+    expect_eq<int, list::first>();
+
+    typedef typename list::rest rest;
+    expect_eq<long, rest::first>;
+
+    typedef typename rest::rest rest_rest;
+    EXPECT_TRUE(rest_rest::empty);
+}

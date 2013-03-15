5abee77147d8261a058f9a57a1a9784219874449
Tianyu Zhu <tian.tian098@gmail.com>
2013-03-15 11:20:05 -0400

    Refactored implementation of helpers to the bottom of the file.

--- include/sky/tuple.hpp
+++ include/sky/tuple.hpp
@@ -10,54 +10,20 @@ namespace sky {
 
 namespace _ {
 
-template <int... Is>
-struct int_list {};
+// Declarations of helper functions/structs //
 
-template <size_t MAX>
-class index_up_to
-{
+template <int... Is> struct int_list {};
 
-    template <size_t I, size_t... List>
-    struct helper
-    {
-        typedef typename helper<I-1, I, List...>::type type;
-    };
-
-    template <size_t... List>
-    struct helper<0, List...>
-    {
-        typedef int_list<0, List...> type;
-
-    };
-
-public:
-
-    typedef typename helper<MAX-1>::type type;
-};
-
-template<>
-struct index_up_to<0>
-{
-    typedef int_list<> type;
-};
+template <size_t MAX> struct index_up_to;
+template <> struct index_up_to<0>;
 
 template <typename F, typename... Args, int... Index>
 void
-invoke_helper_no_return(F &&f, std::tuple<Args...> &args, int_list<Index...>)
-{
-    f(std::forward<
-        typename std::tuple_element<Index, std::tuple<Args...>>::type
-      >(std::get<Index>(args))...);
-}
+invoke_helper_no_return(F&&, std::tuple<Args...>&, int_list<Index...>);
 
 template <typename F, typename... Args, int... Index>
 typename std::result_of<F(Args...)>::type
-invoke_helper_with_return(F &&f, std::tuple<Args...> &args, int_list<Index...>)
-{
-    return f(std::forward<
-                typename std::tuple_element<Index, std::tuple<Args...>>::type
-             >(std::get<Index>(args))...);
-}
+invoke_helper_with_return(F&&, std::tuple<Args...>&, int_list<Index...>);
 
 } // namespace _
 
@@ -83,6 +49,56 @@ invoke(F &&f, std::tuple<Args...> &&args)
     typedef typename index_up_to<sizeof...(Args)>::type Index;
     return invoke_helper_with_return(std::forward<F>(f), args, Index());
 }
+
+namespace _ {
+
+// Implementation of helper declarations //
+
+template<>
+struct index_up_to<0>
+{
+    typedef int_list<> type;
+};
+
+template <size_t MAX>
+struct index_up_to
+{
+private:
+    template <size_t I, size_t... List>
+    struct helper
+    {
+        typedef typename helper<I-1, I, List...>::type type;
+    };
+
+    template <size_t... List>
+    struct helper<0, List...>
+    {
+        typedef int_list<0, List...> type;
+    };
+
+public:
+    typedef typename helper<MAX-1>::type type;
+};
+
+template <typename F, typename... Args, int... Index>
+void
+invoke_helper_no_return(F &&f, std::tuple<Args...> &args, int_list<Index...>)
+{
+    f(std::forward<
+        typename std::tuple_element<Index, std::tuple<Args...>>::type
+      >(std::get<Index>(args))...);
+}
+
+template <typename F, typename... Args, int... Index>
+typename std::result_of<F(Args...)>::type
+invoke_helper_with_return(F &&f, std::tuple<Args...> &args, int_list<Index...>)
+{
+    return f(std::forward<
+                typename std::tuple_element<Index, std::tuple<Args...>>::type
+             >(std::get<Index>(args))...);
+}
+
+} // namespace _
 
 } // namespace sky
 

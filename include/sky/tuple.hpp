c570c0c0ada48b1ea88742e7b6a4ac7395ea1754
Tianyu Zhu <tian.tian098@gmail.com>
2013-03-15 13:00:53 -0400

    Added some docs about invoke

--- include/sky/tuple.hpp
+++ include/sky/tuple.hpp
@@ -27,6 +27,44 @@ invoke_helper_with_return(F&&, std::tuple<Args...>&, int_list<Index...>);
 
 } // namespace _
 
+/** @{
+ * @brief Invokes a function with the parameters given in the tuple.
+ *
+ * Suppose we had a function foo,() which calls an overloaded function bar().
+ * It is possible to write only one definition of foo() that is able to call any
+ * overload of bar() using variadic templates with perfect forwarding:
+ *
+ * template <typename... Args>
+ * void foo(Args &&... args) {
+ *    bar(std::forward<Args>(args)...);
+ * }
+ *
+ * However, if foo() is supposed to call two overloaded functions,
+ * bar1() and bar(2), then it becomes impossible to write foo() using only one
+ * variadic template. A solution would be to use two variadic templates with
+ * the parameters passed in tuples:
+ *
+ * template <typename... Args1, typename Args2>
+ * void foo(std::tuple<Args1...> &&args1, std::tuple<Args2...> &&args2)
+ *
+ * However, there is no standard way of calling bar1() or bar2() with the
+ * arguments stored in tuples. This function provides such a solution:
+ *
+ * invoke(&bar1, args1); // Like bar1(args...)
+ * invoke(&bar2, args2); // Like bar2(args...)
+ *
+ * Furthermore, the return value of invoking bar1() and bar2() can also be used.
+ *
+ * Note: Since foo() takes two tuples instead of one variadic list of arguments,
+ * it should be called in the following way, if perfect forwarding is desired:
+ *
+ *  foo(std::forward_as_tuple(args, for, bar1),
+ *      std::forward_as_tuple(other, args, for, bar, 2));
+ *
+ * @param f The function or functor to invoke.
+ * @param args The arguments of the function, as a tuple.
+ * @return The value the function returns, if any.
+ */
 template <typename F, typename... Args>
 typename std::enable_if<
     std::is_void< typename std::result_of<F(Args...)>::type >::value
@@ -49,6 +87,7 @@ invoke(F &&f, std::tuple<Args...> &&args)
     typedef typename index_up_to<sizeof...(Args)>::type Index;
     return invoke_helper_with_return(std::forward<F>(f), args, Index());
 }
+/// @}
 
 namespace _ {
 

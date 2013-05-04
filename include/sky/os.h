48d3f7d1822e2d73c10263756115d76f8085711c
Tianyu Zhu <tian.tian098@gmail.com>
2013-05-03 20:31:28 -0400

    Added some comments to I/O classes.

--- include/sky/os.h
+++ include/sky/os.h
@@ -5,54 +5,127 @@
 
 namespace sky {
 
+/**
+ * @brief The input end of a file or stream.
+ */
 class input
 {
 public:
+
+    /**
+     * @brief Creates an invalid input object.
+     *
+     * Any calls to write() or close() will throw an exception.
+     */
     input();
+
     input(input const&) = default;
+
     explicit input(int fd);
 
+    /**
+     * @brief Writes the bytes in a given buffer to the input.
+     * @param buf A pointer to the start of the buffer.
+     * @param count The size of the buffer in number of bytes.
+     * @return The number of bytes written to the input.
+     */
     size_t write(void const*buf, size_t count) const;
 
+    /**
+     * @brief Writes the bytes in a given array to the input.
+     * @param buf A reference to an array to write to the buffer.
+     * @return The number of bytes written to the input.
+     */
     template<typename T, size_t N>
     size_t write(T const(&buf)[N]) const
     {
         return write(&buf, N*sizeof(T));
     }
 
+    /**
+     * @brief Writes the bytes of a given object to the input.
+     * @param value The value to write.
+     * @return The number of bytes written to the input.
+     */
     template<typename T>
     size_t write(T const&value) const
     {
         return write(&value, sizeof(T));
     }
 
+    /**
+     * @brief Close this input.
+     *
+     * Once the input is closed, all other calls to write() and close()
+     * will throw std::invalid_argument.
+     */
     void close();
 
 private:
     int fd;
 };
 
+/**
+ * @brief The output end of a stream.
+ */
 class output
 {
 public:
+    /**
+     * @brief Creates an invalid output object.
+     */
     output();
+
     output(output const&) = default;
+
     explicit output(int fd);
 
+    /**
+     * @brief Reads bytes from the output into the given buffer.
+     * @param buf A pointer to the start of the buffer.
+     * @param count The size of the buffer in number of bytes.
+     * @return The number of bytes read.
+     */
     size_t read(void *buf, size_t count) const;
 
+    /**
+     * @brief Read bytes from the output into the given array.
+     *
+     * It is up to the user to ensure that the number of bytes and the type of
+     * data available to be read aligns with elements of the array.
+     * It is highly recommended that only POD types are used for this function.
+     *
+     * @param buf The array to store the read bytes.
+     * @return The number of bytes read.
+     */
     template<typename T, size_t N>
     size_t read(T (&buf)[N]) const
     {
         return read(&buf, N*sizeof(T));
     }
 
+    /**
+     * @brief Read bytes from the output into a given value.
+     *
+     * It is up to the suer to ensure that the number of bytes and the type of
+     * data available to be read can be interpreted as an object of type T.
+     * It is highly recommended that only POD types are used for this function.
+     *
+     * @param value The value to store the read bytes.
+     * @return The number of bytes read.
+     */
     template<typename T>
     size_t read(T &value) const
     {
         return read(&value, sizeof(T));
     }
 
+    /**
+     * @brief Close this output.
+     *
+     * Once the output is closed, all other calls to read() and close() will
+     * throw std::invalid_argument.
+     */
     void close();
 
 private:

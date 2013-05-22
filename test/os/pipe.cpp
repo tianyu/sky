5aa2ee6ae50262a018e7b585b078de04fa45ac0f
Tianyu Zhu <tian.tian098@gmail.com>
2013-05-22 15:52:29 -0400

    Better unit test for pipe

--- test/os/pipe.cpp
+++ test/os/pipe.cpp
@@ -1,5 +1,7 @@
 #include "gtest/gtest.h"
 
+#include "sky/scope_guard.hpp"
+
 #include "sky/os.h"
 
 using namespace sky;
@@ -7,4 +9,16 @@ using namespace sky;
 TEST(Pipe, Make)
 {
     auto pipe = make_pipe();
+    input &in = get<0>(pipe);
+    output &out = get<1>(pipe);
+    auto close_in = scope_guard([&] { in.close(); });
+    auto close_out = scope_guard([&] { out.close(); });
+
+    char const expected[] { "Hello" };
+    char actual[10] { '\0' };
+
+    ASSERT_EQ(6, output.write(expected));
+    ASSERT_EQ(6, input.read(actual));
+
+    EXPECT_STREQ(expected, actual);
 }

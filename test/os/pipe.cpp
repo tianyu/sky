e7acef7090e5156016972f2cd7f63474fce82600
Tianyu Zhu <tian.tian098@gmail.com>
2013-05-22 16:34:10 -0400

    [build] Fixed build issues.

--- test/os/pipe.cpp
+++ test/os/pipe.cpp
@@ -9,16 +9,16 @@ using namespace sky;
 TEST(Pipe, Make)
 {
     auto pipe = make_pipe();
-    input &in = get<0>(pipe);
-    output &out = get<1>(pipe);
+    input &in = std::get<0>(pipe);
+    output &out = std::get<1>(pipe);
     auto close_in = scope_guard([&] { in.close(); });
     auto close_out = scope_guard([&] { out.close(); });
 
     char const expected[] { "Hello" };
     char actual[10] { '\0' };
 
-    ASSERT_EQ(6, output.write(expected));
-    ASSERT_EQ(6, input.read(actual));
+    ASSERT_EQ(6, out.write(expected));
+    ASSERT_EQ(6, in.read(actual));
 
     EXPECT_STREQ(expected, actual);
 }

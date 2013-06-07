cd35eabbc3dd0450b3a936b945a38e6aba0eab5f
Tianyu Zhu <tian.tian098@gmail.com>
2013-06-07 17:09:54 -0400

    Started unit tests.

--- test/type_traits/relate.cpp
+++ test/type_traits/relate.cpp
@@ -4,4 +4,24 @@
 
 using namespace sky;
 
+namespace {
 
+struct same
+{
+    template<typename T, typename U>
+    using op = std::is_same<T,U>;
+};
+
+} // namespace
+
+TEST(Relate, Default_True)
+{
+    bool value = relate<same>::template op<int, int>::value;
+    EXPECT_TRUE(value);
+}
+
+TEST(Relate, Default_False)
+{
+    bool value = relate<same>::template op<int, double>::value;
+    EXPECT_FALSE(value);
+}

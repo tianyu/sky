685042221b5071df04a6362c76d8a39e1e2f3890
Tianyu Zhu <tian.tian098@gmail.com>
2013-06-08 14:29:24 -0400

    Relate coming along nicely.
    Still need to implement/test using forall/exists for both arguments.

--- include/sky/type_traits.hpp
+++ include/sky/type_traits.hpp
@@ -101,11 +101,61 @@ struct forall {};
 template<typename Predicate>
 struct relate
 {
+private:
+
     template<typename T, typename U>
-    struct op : public Predicate::template op<T,U>
+    struct op_base : public Predicate::template op<T,U>
     {};
 
+    template<typename T>
+    struct op_base<T, forall<>> : public std::true_type
+    {};
 
+    template<typename T, typename U, typename... Us>
+    struct op_base<T, forall<U, Us...>> : public
+        std::integral_constant<bool,
+            Predicate::template op<T,U>::value &&
+            op_base<T, forall<Us...>>::value>
+    {};
+
+    template<typename T>
+    struct op_base<T, exists<>> : public std::false_type
+    {};
+
+    template<typename T, typename U, typename... Us>
+    struct op_base<T, exists<U, Us...>> : public
+        std::integral_constant<bool,
+            Predicate::template op<T,U>::value ||
+            op_base<T, exists<Us...>>::value>
+    {};
+
+public:
+
+    template<typename T, typename U>
+    struct op : public op_base<T, U>
+    {};
+
+    template<typename U>
+    struct op<forall<>, U> : public std::true_type
+    {};
+
+    template<typename T, typename... Ts, typename U>
+    struct op<forall<T, Ts...>, U> : public
+        std::integral_constant<bool,
+            Predicate::template op<T,U>::value &&
+            op<forall<Ts...>, U>::value>
+    {};
+
+    template<typename U>
+    struct op<exists<>, U> : public std::false_type
+    {};
+
+    template<typename T, typename... Ts, typename U>
+    struct op<exists<T, Ts...>, U> : public
+        std::integral_constant<bool,
+            Predicate::template op<T,U>::value ||
+            op<exists<Ts...>, U>::value>
+    {};
 
 };
 
--- test/type_traits/relate.cpp
+++ test/type_traits/relate.cpp
@@ -25,3 +25,138 @@ TEST(Relate, Default_False)
     bool value = relate<same>::template op<int, double>::value;
     EXPECT_FALSE(value);
 }
+
+TEST(Relate, T_Forall_None)
+{
+    bool value = relate<same>::template op<int, forall<>>::value;
+    EXPECT_TRUE(value);
+}
+
+TEST(Relate, T_Forall_U_True)
+{
+    bool value = relate<same>::template op<int, forall<int>>::value;
+    EXPECT_TRUE(value);
+}
+
+TEST(Relate, T_Forall_U_False)
+{
+    bool value = relate<same>::template op<int, forall<double>>::value;
+    EXPECT_FALSE(value);
+}
+
+TEST(Relate, T_Forall_Us_True)
+{
+    bool value = relate<same>::
+            template op<int, forall<int, int, int>>::value;
+    EXPECT_TRUE(value);
+}
+
+TEST(Relate, T_Forall_Us_False)
+{
+    bool value = relate<same>::
+            template op<int, forall<int, double, int>>::value;
+    EXPECT_FALSE(value);
+}
+
+TEST(Relate, T_Exists_None)
+{
+    bool value = relate<same>::template op<int, exists<>>::value;
+    EXPECT_FALSE(value);
+}
+
+TEST(Relate, T_Exists_U_True)
+{
+    bool value = relate<same>::template op<int, exists<int>>::value;
+    EXPECT_TRUE(value);
+}
+
+TEST(Relate, T_Exists_U_False)
+{
+    bool value = relate<same>::template op<int, exists<double>>::value;
+    EXPECT_FALSE(value);
+}
+
+TEST(Relate, T_Exists_Us_True)
+{
+    bool value = relate<same>::
+            template op<int, exists<double, int, double>>::value;
+    EXPECT_TRUE(value);
+}
+
+TEST(Relate, T_Exists_Us_False)
+{
+    bool value = relate<same>::
+            template op<int, exists<double, double, double>>::value;
+    EXPECT_FALSE(value);
+}
+
+TEST(Relate, Forall_None_U)
+{
+    bool value = relate<same>::template op<forall<>, int>::value;
+    EXPECT_TRUE(value);
+}
+
+TEST(Relate, Forall_T_U_True)
+{
+    bool value = relate<same>::template op<forall<int>, int>::value;
+    EXPECT_TRUE(value);
+}
+
+TEST(Relate, Forall_T_U_False)
+{
+    bool value = relate<same>::template op<forall<double>, int>::value;
+    EXPECT_FALSE(value);
+}
+
+TEST(Relate, Forall_Ts_U_True)
+{
+    bool value = relate<same>::
+            template op<forall<int, int, int>, int>::value;
+    EXPECT_TRUE(value);
+}
+
+TEST(Relate, Forall_Ts_U_False)
+{
+    bool value = relate<same>::
+            template op<forall<int, double, int>, int>::value;
+    EXPECT_FALSE(value);
+}
+
+TEST(Relate, Exists_None_U)
+{
+    bool value = relate<same>::template op<exists<>, int>::value;
+    EXPECT_FALSE(value);
+}
+
+TEST(Relate, Exists_T_U_True)
+{
+    bool value = relate<same>::template op<exists<int>, int>::value;
+    EXPECT_TRUE(value);
+}
+
+TEST(Relate, Exists_T_U_False)
+{
+    bool value = relate<same>::template op<exists<double>, int>::value;
+    EXPECT_FALSE(value);
+}
+
+TEST(Relate, Exists_Ts_U_True)
+{
+    bool value = relate<same>::
+            template op<exists<double, int, double>, int>::value;
+    EXPECT_TRUE(value);
+}
+
+TEST(Relate, Exists_Ts_U_False)
+{
+    bool value = relate<same>::
+            template op<exists<double, double, double>, int>::value;
+    EXPECT_FALSE(value);
+}
+
+TEST(Relate, ForAll_None_ForAll_None)
+{
+    bool value = relate<same>::
+            template op<forall<>, forall<>>::value;
+    EXPECT_TRUE(value);
+}

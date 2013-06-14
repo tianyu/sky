a2ea7be6e9871090fe19e5cd380b7aaada91bb7f
Tianyu Zhu <tian.tian098@gmail.com>
2013-06-14 16:39:43 -0400

    Another reimplementation of relate potentially allows unary types.

--- include/sky/type_traits.hpp
+++ include/sky/type_traits.hpp
@@ -45,6 +45,47 @@ struct exists {};
 template<typename... Ts>
 struct forall {};
 
+template<typename Predicate, typename T>
+struct curry
+{
+    template<typename... Ts>
+    struct op : public Predicate::template op<T, Ts...>
+    {};
+};
+
+template<typename Predicate, typename... Args>
+struct _relate;
+
+template<typename Predicate>
+struct _relate<Predicate> : public Predicate::template op<>
+{};
+
+template<typename Predicate, typename Arg, typename... Args>
+struct _relate<Predicate, Arg, Args...> : public _relate<curry<Predicate, Arg>, Args...>
+{};
+
+template<typename Predicate, typename... Args>
+struct _relate<Predicate, forall<>, Args...> : public std::true_type
+{};
+
+template<typename Predicate, typename T, typename... Ts, typename... Args>
+struct _relate<Predicate, forall<T, Ts...>, Args...> : public
+        std::integral_constant<bool,
+        _relate<Predicate, T, Args...>::value &&
+        _relate<Predicate, forall<Ts...>, Args...>::value>
+{};
+
+template<typename Predicate, typename... Args>
+struct _relate<Predicate, exists<>, Args...> : std::false_type
+{};
+
+template<typename Predicate, typename T, typename... Ts, typename... Args>
+struct _relate<Predicate, exists<T, Ts...>, Args...> : public
+        std::integral_constant<bool,
+        _relate<Predicate, T, Args...>::value ||
+        _relate<Predicate, exists<Ts...>, Args...>::value>
+{};
+
 template<typename Predicate>
 struct relate
 {

0b02d0272c19bfd8e9f45dd6441ac745af8157c2
Tianyu Zhu <tian.tian098@gmail.com>
2013-06-28 12:42:59 -0400

    Initial docs for array.

--- include/sky/array.hpp
+++ include/sky/array.hpp
@@ -1,4 +1,28 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <array>
+
+namespace sky {
+
+/**
+ * sky::array is a container that encapsulates multi-dimensional arrays.
+ *
+ * Thus struct has the same aggregate type semantics as a C-style array.
+ * The size and efficienty of an m-dimensional array,
+ * `array<T, N1, N2, ..., Nm>`, is equivalent to the size and efficiency of
+ * `T[N1*N2*...*Nm]`.
+ * The struct provides the benefits of a standard container, such as knowing its
+ * own size, supporting assignment, random access iterators, etc.
+ *
+ * In the special case where some dimension `Ni == 0`,
+ * `array.begin() == array.end()`, which is some unique value.
+ * The effect of calling `front()` or `back()` on such an array is undefined.
+ *
+ * `array` is an aggregate (it has no user-defined constructors and no private
+ * or protected members), which allows it to use aggregate-initialization.
+ */
+
+} // namespace sky
+
 #endif // ARRAY_HPP

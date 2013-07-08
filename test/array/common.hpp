a4168aa310af7490f46d285f8968182fd7e9fbef
Tianyu Zhu <tian.tian098@gmail.com>
2013-07-08 13:34:44 -0400

    Added common.hpp for array tests.

---
+++ test/array/common.hpp
@@ -0,0 +1,73 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <array>
+
+#include "sky/type_list.hpp"
+
+#include "sky/array.hpp"
+
+namespace  {
+
+template<std::size_t... Ns> struct Param
+{
+private:
+    template<std::size_t... Vals>
+    static std::array<int, sizeof...(Vals)>
+    make_array_helper(sky::index_list<Vals...>)
+    {
+        return std::array<int, sizeof...(Vals)>{{ Vals... }};
+    }
+
+public:
+
+    using array_t = sky::array<int, Ns...>;
+
+    static const std::size_t size = sky::product<sky::index_list<Ns...>>::value;
+
+    static const bool is_empty = (size == 0);
+
+    static const int front = 1;
+
+    static const int back = size;
+
+    static int const* begin_of(array_t const&array)
+    {
+        return reinterpret_cast<int const*>(&array);
+    }
+
+    static int const* end_of(array_t const&array)
+    {
+        return begin_of(array) + size;
+    }
+
+    static int const* rbegin_of(array_t const&array)
+    {
+        return end_of(array) - 1;
+    }
+
+    static int const* rend_of(array_t const&array)
+    {
+        return begin_of(array) - 1;
+    }
+
+    static array_t make_array()
+    {
+        using values_t = typename sky::index_range<1, size + 1>::type;
+        // This reinterpret cast should work since both sky::array and
+        // std::array should have the same layout.
+        return reinterpret_cast<array_t&&>(
+                    std::move(make_array_helper(values_t())));
+    }
+
+};
+
+template<typename T>
+constexpr bool is_const(T &) { return false; }
+
+template<typename T>
+constexpr bool is_const(T const&) { return true; }
+
+} // namespace
+
+#endif // COMMON_HPP
--- test/array/members.cpp
+++ test/array/members.cpp
@@ -1,80 +1,10 @@
-#include <array>
-#include <iterator>
-#include <type_traits>
-#include <utility>
-
 #include "gtest/gtest.h"
 
-#include "sky/type_list.hpp"
-
-#include "sky/array.hpp"
-
-namespace  {
-
-template<std::size_t... Ns> struct Param
-{
-private:
-    template<std::size_t... Vals>
-    static std::array<int, sizeof...(Vals)>
-    make_array_helper(sky::index_list<Vals...>)
-    {
-        return std::array<int, sizeof...(Vals)>{{ Vals... }};
-    }
-
-public:
-
-    using array_t = sky::array<int, Ns...>;
-
-    static const std::size_t size = sky::product<sky::index_list<Ns...>>::value;
-
-    static const bool is_empty = (size == 0);
-
-    static const int front = 1;
-
-    static const int back = size;
-
-    static int const* begin_of(array_t const&array)
-    {
-        return reinterpret_cast<int const*>(&array);
-    }
-
-    static int const* end_of(array_t const&array)
-    {
-        return begin_of(array) + size;
-    }
-
-    static int const* rbegin_of(array_t const&array)
-    {
-        return end_of(array) - 1;
-    }
-
-    static int const* rend_of(array_t const&array)
-    {
-        return begin_of(array) - 1;
-    }
-
-    static array_t make_array()
-    {
-        using values_t = typename sky::index_range<1, size + 1>::type;
-        // This reinterpret cast should work since both sky::array and
-        // std::array should have the same layout.
-        return reinterpret_cast<array_t&&>(
-                    std::move(make_array_helper(values_t())));
-    }
-
-};
+#include "common.hpp"
 
 template<typename P>
 struct Array_Member : public ::testing::Test
 {};
-
-template<typename T>
-constexpr bool is_const(T &) { return false; }
-
-template<typename T>
-constexpr bool is_const(T const&) { return true; }
-
-} // namespace
 
 using Types = ::testing::Types<
     Param<>,

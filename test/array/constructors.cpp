31e1b4d7cd814b3a940fcf1b9164fc5625698343
Tianyu Zhu <tian.tian098@gmail.com>
2013-07-22 20:22:39 -0400

    Added more constructor tests + size tests

--- test/array/constructors.cpp
+++ test/array/constructors.cpp
@@ -32,61 +32,92 @@ void expect_sequence_constructed(sky::array<int, Ns...> const&array)
 
 } // namespace
 
+TEST(Array_Construct, Dim0_Size)
+{
+    EXPECT_EQ(sizeof(int), sizeof(sky::array<int>));
+}
+
 TEST(Array_Construct, Dim0_Default)
 {
     auto array = sky::array<int>{};
-
     expect_default_constructed(array);
 }
 
 TEST(Array_Construct, Dim0_Aggregate)
 {
-    auto array = sky::array<int>{1};
+    auto array1 = sky::array<int>{1};
+    expect_sequence_constructed(array1);
 
-    expect_sequence_constructed(array);
+    auto array2 = sky::array<int>{{}};
+    expect_default_constructed(array2);
+}
+
+TEST(Array_Construct, Dim1_Size)
+{
+    EXPECT_EQ(2*sizeof(int), sizeof(sky::array<int,2>));
+    EXPECT_EQ(0, sizeof(sky::array<int,0>));
 }
 
 TEST(Array_Construct, Dim1_Default)
 {
     auto array = sky::array<int, 2>{};
-
     expect_default_constructed(array);
 }
 
 TEST(Array_Construct, Dim1_Aggregate)
 {
-    auto array = sky::array<int, 2>{{1, 2}};
+    auto array1 = sky::array<int, 2>{{1, 2}};
+    expect_sequence_constructed(array1);
 
-    expect_sequence_constructed(array);
+    auto array2 = sky::array<int, 2>{{{}, {}}};
+    expect_default_constructed(array2);
+}
+
+TEST(Array_Construct, Dim2_Size)
+{
+    EXPECT_EQ(4*sizeof(int), sizeof(sky::array<int, 2, 2>));
+    EXPECT_EQ(0, sizeof(sky::array<int, 0, 2>));
+    EXPECT_EQ(0, sizeof(sky::array<int, 2, 0>));
 }
 
 TEST(Array_Construct, Dim2_Default)
 {
     auto array = sky::array<int, 2, 2>{};
-
     expect_default_constructed(array);
 }
 
 TEST(Array_Construct, Dim2_Aggregate)
 {
-    auto array = sky::array<int, 2, 2>{{
+    auto array1 = sky::array<int, 2, 2>{{
         {{1, 2}},
         {{3, 4}}
     }};
+    expect_sequence_constructed(array1);
 
-    expect_sequence_constructed(array);
+    auto array2 = sky::array<int, 2, 2>{{
+        {{ {}, {} }},
+        {{ {}, {} }}
+    }};
+    expect_default_constructed(array2);
+}
+
+TEST(Array_Construct, Dim3_Size)
+{
+    EXPECT_EQ(12*sizeof(int), sizeof(sky::array<int, 2, 3, 2>));
+    EXPECT_EQ(0, sizeof(sky::array<int, 2, 3, 0>));
+    EXPECT_EQ(0, sizeof(sky::array<int, 2, 0, 3>));
+    EXPECT_EQ(0, sizeof(sky::array<int, 0, 3, 2>));
 }
 
 TEST(Array_Construct, Dim3_Default)
 {
     auto array = sky::array<int, 2, 3, 2>{};
-
     expect_default_constructed(array);
 }
 
 TEST(Array_Construct, Dim3_Aggregate)
 {
-    auto array = sky::array<int, 2, 3, 2>{{
+    auto array1 = sky::array<int, 2, 3, 2>{{
         {{
             {{1, 2}},
             {{3, 4}},
@@ -97,6 +128,18 @@ TEST(Array_Construct, Dim3_Aggregate)
             {{11, 12}}
         }}
     }};
+    expect_sequence_constructed(array1);
 
-    expect_sequence_constructed(array);
+    auto array2 = sky::array<int, 2, 3, 2>{{
+        {{
+            {{ {}, {} }},
+            {{ {}, {} }},
+            {{ {}, {} }}
+        }}, {{
+            {{ {}, {} }},
+            {{ {}, {} }},
+            {{ {}, {} }}
+        }}
+    }};
+    expect_default_constructed(array2);
 }
